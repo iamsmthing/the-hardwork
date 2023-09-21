d8f543c09d27988f392b4c492b3972d1099f5430
iamsmthing <shivamkumarroy938@gmail.com>
2023-09-21 23:42:41 +0530

    ignored eslint and added key prop

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,14 +4,16 @@ import logo from "@/assets/images/home.png";
 import SidebarLink from "./SidebarLink";
 
 const links = [
-  { label: "Home", icon: "Grid", link: "/home" },
+  { id: 1, label: "Home", icon: "Grid", link: "/home" },
   {
+    id: 2,
     label: "Calendar",
     icon: "Calendar",
     link: "/calendar",
   },
-  { label: "Profile", icon: "User", link: "/profile" },
+  { id: 3, label: "Profile", icon: "User", link: "/profile" },
   {
+    id: 4,
     label: "Settings",
     icon: "LogOut",
     link: "/api/logout",
@@ -25,7 +27,7 @@ const Sidebar = () => {
         <Image src={logo} alt="Able logo" priority className="w-14" />
       </div>
       {links.map((link) => (
-        <SidebarLink link={link} />
+        <SidebarLink link={link} key={link.id} />
       ))}
     </Card>
   );
