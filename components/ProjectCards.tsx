40363fc6dad0950e042427e554d538c7a8437680
iamsmthing <shivamkumarroy938@gmail.com>
2023-09-21 23:45:35 +0530

    added react components

--- components/ProjectCards.tsx
+++ components/ProjectCards.tsx
@@ -1,20 +1,7 @@
-import { FC } from "react";
-import { Prisma } from "@prisma/client";
-import Card from "./Card";
-import clsx from "clsx";
+import React from "react";
 
-const projectWithTasks = Prisma.validator<Prisma.ProjectDefaultArgs>()({
-  include: { tasks: true },
-});
-
-type ProjectWithTasks = Prisma.ProjectGetPayload<typeof projectWithTasks>;
-
-const format = (date: string | number | Date) => {
-  new Date(date).toLocaleDateString("en-us", {
-    weekday: "long",
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
+const ProjectCards = () => {
+  return <div>ProjectCards</div>;
 };
-const ProjectCard: FC<{ project: ProjectWithTasks }> = ({ project }) => {};
+
+export default ProjectCards;
