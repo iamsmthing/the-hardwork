2a4187f0ad655ba3bef7aa4cba14c809a01d9ed1
iamsmthing <shivamkumarroy938@gmail.com>
2023-09-24 19:21:35 +0530

    input validation

--- components/Input.tsx
+++ components/Input.tsx
@@ -3,6 +3,8 @@ import clsx from "clsx";
 const Input = ({ className, ...props }) => {
   return (
     <input
+      required
+      min={5}
       className={clsx(
         "border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full",
         className
